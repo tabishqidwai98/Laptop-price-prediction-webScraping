4d50445ff3ad48e956fb6bd163506711145ba08a
Tabish <tabish786qidwai@gmail.com>
2024-05-06 13:42:02 +0530

    Updated tags, class, links, dataframe
    updated the attributes and tags.
    refined the links,
    maintained the dataset, CSV

--- price_scraper_v1.py
+++ price_scraper_v1.py
@@ -25,7 +25,7 @@ def get_title(soup):
 def get_price(soup):
 
   try:
-    price = soup.find("span", attrs={"class":'a-price aok-align-center reinventPricePriceToPayMargin priceToPay'}).find("span", attrs={"class": "a-offscreen"}).text
+    price = soup.find("span", attrs={"class":'a-price-whole'}).text.strip()
 
   except AttributeError:
     price = " "
@@ -36,93 +36,111 @@ def get_price(soup):
 def get_rating(soup):
 
   try:
-    rating = soup.find("i", attrs={'class':'a-icon a-icon-star a-icon-4-5'}).string.strip()
+    rating = soup.find("div", attrs={'id':'averageCustomerReviews_feature_div'}).find('div').find('i').string.strip()
 
   except AttributeError:
-    try:
-      rating = soup.find("span", attrs={'class':'a-icon-alt'}).string.strip()
-    except:
-      rating = " "
+    rating = " "
 
   return rating
 
 # Function to extract number of user reviews
 def get_review_count(soup):
   try:
-    review_count = soup.find("span", attrs={'class':'a-size-base s-underline-text'}).string.strip()
+    review_count = soup.find('a', attrs={'id' : 'acrCustomerReviewLink'}).find('span').string.strip()
 
   except AttributeError:
     review_count = " "
 
   return review_count
 
-# Function to extract avalability status
+# Function to extract availability status
 def get_availability(soup):
   try:
-    available = soup.find("div", attrs={'id':'availability'})
-    available = available.find("span").string.strip()
-
+    available = soup.find('div', attrs = {'id': 'availability'}).find('span').text.strip()
+    
   except AttributeError:
-    available = "Not Available"
+    available = soup.find('span', attrs={'class' : 'a-size-medium a-color-success'}).text.strip()
+    if not available:
+      available = soup.find('span', attrs = {'id':'submit.add-to-cart-announce'}).text.strip()
+      available = "In stock" if available else "Not Available"
+    else:
+      available = "Not Available"
 
   return available
 
 # The webpage URL
 def get_amazon_data(url, HEADERS): 
-    # HTTP Request
-    webpage = requests.get(url, headers=HEADERS)
-    print(f'webpage status code: {webpage.status_code}')
-    # Soup object containing all data
-    soup = BeautifulSoup(webpage.content, "html.parser")
-    # Fetch links as List of Tag Objects
-    links = soup.find_all("a", attrs={'class':'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'})
-    # Store the links
-    links_list = []
+  # HTTP Request
+  webpage = requests.get(url, headers=HEADERS)
+  print(f'webpage status code: {webpage.status_code}')
+  # Soup object containing all data
+  soup = BeautifulSoup(webpage.content, "html.parser")
+  # Fetch links as List of Tag Objects
+  links = soup.find_all("a", attrs={'class':'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'})
+  # Store the links
+  links_list = []
+  
+  # log the links for use
 
-    # Loop for extracting links from tag objects
-    for link in links:
-        links_list.append(link.get('href'))
+  # Loop for extracting links from tag objects
+  # refined the links_list removing the sponsored ads
+  for link in links:
+    i = link.get('href')
+    if i[0:5] == 'https' or i[0:5] == '/sspa':
+        continue
+    else:
+        links_list.append(i)
+    
+    #links_list.append(link.get('href'))"""
 
-    d = {"title":[], "price":[], "rating":[], "reviews":[], "availability":[]}
 
-    # Loop for extracting product details from each link
-    for link in links_list:
-        print(f'=> visiting {link}')
-        try:
-            new_webpage = requests.get("https://www.amazon.in" + link, headers=HEADERS)
-            new_soup = BeautifulSoup(new_webpage.content, "html.parser")
+  d = {"title":[], "price":[], "rating":[], "reviews":[], "availability":[], 'links' : [] }
 
-            # Function calls to display all necessary product information
-            d['title'].append(get_title(new_soup))
-            d['price'].append(get_price(new_soup))
-            d['rating'].append(get_rating(new_soup))
-            d['reviews'].append(get_review_count(new_soup))
-            d['availability'].append(get_availability(new_soup))
-        except:
-            print('=> error occured')
-            pass
-    amazon_df = pd.DataFrame.from_dict(d)
-    amazon_df['title'].replace('', np.nan, inplace=True)
-    amazon_df = amazon_df.dropna(subset=['title'])
-    return amazon_df
+  # Loop for extracting product details from each link
+  for link in links_list:
+    try:
+      print('=> visiting', f'https://www.amazon.in{link}')
+      new_webpage = requests.get("https://www.amazon.in" + link, headers=HEADERS)
+      
+      new_soup = BeautifulSoup(new_webpage.content, "html.parser")
+
+      # Function calls to display all necessary product information
+      # link to each extracted data for use/reference
+      d['title'].append(get_title(new_soup))
+      d['price'].append(get_price(new_soup))
+      d['rating'].append(get_rating(new_soup))
+      d['reviews'].append((get_review_count(new_soup)))
+      d['availability'].append(get_availability(new_soup))
+      d['links'].append(f'https://www.amazon.in{link}')
+
+    except:
+      print('=> error occured')
+      pass
+  
+  amazon_df = pd.DataFrame.from_dict(d)
+  amazon_df['title'] = amazon_df['title'].replace('', np.nan)
+  amazon_df = amazon_df.dropna(subset=['title'])
+  return amazon_df
 
 def collect_all(q='laptop', pos=1):
   # Headers for request
-  HEADERS = ({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
+  HEADERS = ({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
   results = []
   while True:
-    print(f'Collecting data from amazon for {q} from page {pos}')
+    print(f'Collecting data from amazon for {q} : {pos}')
     amazon_url = f"https://www.amazon.in/s?k={q}&page={pos}"
     print(amazon_url)
     out = get_amazon_data(amazon_url, HEADERS)
     print(out.shape)
-    if len(out) > 0:
+    if len(out) > 0: #and pos<=2:
       pos+=1
       results.append(out)
       print('results updated')
     else:
       print(len(out), 'items found')
       break
+
+  #print('total nomber of pages visited = Size of the collected data = ', len(results)) # help understanding
   return results
 
 def save_data(data, filename):
